12d56f77f3ee63a39a0402ee66532d0c6670ca42
Deanna Stevens <deanna.sofias@gmail.com>
2023-05-17 19:34:02 -0400

    Add methods and indexes

--- arrays.js
+++ arrays.js
@@ -1,3 +1,25 @@
 var friends = ["Cory" , "Joseph" ,"Katie" , "Peggy"];
+/* This method will remove the last element, 
+return the element in the console output, and also remove the element
+from the array 
+Index positions: Cory = 0, Joseph = 1, Katie = 2, Peggy = 3 
+*/
+friends.pop();
+console.log(friends);
+
 var monthlyUtilities = [32, 78, 50, 65]
+/* This method will remove the first element,
+change the length of the array, and return the removed element 
+in the console output
+Index positions: 32 = 0, 78 = 1, 50 = 2, 65 = 3 
+*/
+monthlyUtilities.shift();
+console.log(monthlyUtilities);
+
 var sunny = [true, false, true, false]
+/* This method will add the element(s) in paratheses after '.push'
+to the end of the array and return the new value of the array
+Index positions: true = 0, false = 1, true = 2, false = 3
+ */
+sunny.push(false);
+console.log(sunny);
